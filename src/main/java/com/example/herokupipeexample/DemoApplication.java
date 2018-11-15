71e0e8fb43fecb1c128a007265ec6b94f48ba166
examuser <exam@cmarker.no>
2018-11-15 20:32:48 +0100

    x

--- src/main/java/com/example/herokupipeexample/DemoApplication.java
+++ src/main/java/com/example/herokupipeexample/DemoApplication.java
@@ -2,9 +2,35 @@ package com.example.herokupipeexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
+import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Configuration
 @SpringBootApplication
 public class DemoApplication {
+	
+	@Bean
+	@Primary
+	public DataSource dataSource() throws URISyntaxException {
+		
+		URI dbUri = new URI(System.getenv("DATABASE_URL"));
+		
+		String username = dbUri.getUserInfo().split(":")[0];
+		String password = dbUri.getUserInfo().split(":")[1];
+		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ":" + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
+		return DataSourceBuilder.create()
+				.url(dbUrl)
+				.username(username)
+				.password(password)
+				.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
