e1bd58a976b70cfc1b8488ead7eced121b22c719
examuser <exam@cmarker.no>
2018-11-15 23:23:10 +0100

    x

--- src/main/java/com/example/herokupipeexample/DemoApplication.java
+++ src/main/java/com/example/herokupipeexample/DemoApplication.java
@@ -15,12 +15,22 @@ import java.net.URISyntaxException;
 @SpringBootApplication
 public class DemoApplication {
 	
+	
+	/*
+	URI:        postgres://cynzpiozskmril:0a48d79a9fa96a872915cb5eb1b920b273bd7022ad269fbf5071805c12908ba6@ec2-79-125-124-30.eu-west-1.compute.amazonaws.com:5432/dcbtgk1u06j8eo
+	USER:       cynzpiozskmril
+	PASSWORD:   0a48d79a9fa96a872915cb5eb1b920b273bd7022ad269fbf5071805c12908ba6
+	 */
+	
 	@Bean
 	@Primary
 	public DataSource dataSource() throws URISyntaxException {
 		
 		URI dbUri = new URI(System.getenv("DATABASE_URL"));
 		System.out.println("Uri: " + dbUri.toString());
+		System.out.println("Uri.host: " + dbUri.getPath());
+		System.out.println("Uri.port " + dbUri.getPort());
+		System.out.println("Uri.path: " + dbUri.getPath());
 		String username = dbUri.getUserInfo().split(":")[0];
 		System.out.println("Username: " + username);
 		String password = dbUri.getUserInfo().split(":")[1];
