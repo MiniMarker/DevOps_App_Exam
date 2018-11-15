60691c93b5ac4b9f81edc0c73a405682745d1ba0
examuser <exam@cmarker.no>
2018-11-15 13:01:50 +0100

    imported test dependencies and created a positive test

---
+++ src/test/java/com/example/herokupipeexample/TestBasic.java
@@ -0,0 +1,14 @@
+package com.example.herokupipeexample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestBasic {
+	
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+	
+}
