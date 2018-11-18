4406ac3b785a251554c99bd0e29745c75a6277c3
examuser <exam@cmarker.no>
2018-11-18 12:44:15 +0100

    revert properties and added random timer on homepage

--- src/main/java/com/example/herokupipeexample/CustomerController.java
+++ src/main/java/com/example/herokupipeexample/CustomerController.java
@@ -1,20 +1,18 @@
 package com.example.herokupipeexample;
 
 import java.util.List;
+import java.util.Random;
 
 import com.codahale.metrics.Meter;
 import com.codahale.metrics.MetricRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CustomerController {
 
     private CustomerRepository customerRepository;
+	private Random r;
 
 	@Autowired
 	public MetricRegistry metricRegistry;
@@ -22,13 +20,28 @@ public class CustomerController {
     @Autowired
     public CustomerController(CustomerRepository customerRepository, MetricRegistry mark) {
       this.customerRepository = customerRepository;
+       this.r = new Random();
       
     }
 
     @RequestMapping("/")
     public String welcome() {
-    	metricRegistry.meter("WelcomePageCount").mark();
-        return "Welcome to this small REST service. It will accept a GET on /list with a request parameter lastName, and a POST to / with a JSON payload with firstName and lastName as values.";
+    	
+	    int time = r.nextInt(3000);
+	
+	    metricRegistry.timer("WelcomePageLoadTimer").time();
+	
+	    try {
+		    sleep(time);
+		    
+		    return "Welcome to this small REST service. It will accept a " +
+				    "GET on /list with a request parameter lastName, and a " +
+				    "POST to / with a JSON payload with firstName and lastName as values.";
+		    
+	    } finally {
+		    metricRegistry.meter("WelcomePageCount").mark();
+	    	metricRegistry.timer("WelcomePageLoadTimer").time().stop();
+	    }
     }
 
     @RequestMapping("/list")
@@ -40,6 +53,14 @@ public class CustomerController {
     public Customer newCustomer(@RequestBody Customer customer) {
         System.out.println(customer);
         return customerRepository.save(customer);
+    }
+    
+    private void sleep(int x) {
+    	try {
+    		Thread.sleep(x);
+	    } catch (InterruptedException e) {
+		    e.printStackTrace();
+	    }
     }
 
 }
