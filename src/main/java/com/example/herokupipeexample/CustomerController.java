851791e5a4ebd1f34f3a6308e8a7df4f0ed8282e
examuser <exam@cmarker.no>
2018-11-27 12:21:16 +0100

    added metrics to app

--- src/main/java/com/example/herokupipeexample/CustomerController.java
+++ src/main/java/com/example/herokupipeexample/CustomerController.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.Random;
 
 
+import com.codahale.metrics.Meter;
 import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -82,19 +84,30 @@ public class CustomerController {
 	private CustomerRepository customerRepository;
 	private Random r;
 	
+	private Meter welcomePageCount;
+	private Timer welcomePageTimer;
+	
 	@Autowired
 	public MetricRegistry metricRegistry;
 	
 	public CustomerController(CustomerRepository customerRepository) {
 		this.customerRepository = customerRepository;
 		this.r = new Random();
+		
+		configureMetrics(metricRegistry);
+		
+	}
+	
+	private void configureMetrics(MetricRegistry metricRegistry) {
+		welcomePageCount = metricRegistry.meter(MetricRegistry.name("welcomePageCount"));
+		welcomePageTimer = metricRegistry.timer("welcomePageTimer");
 	}
 	
 	@RequestMapping("/")
 	public String welcome() {
 		
 		int time = r.nextInt(3000);
-		metricRegistry.timer("WelcomePageLoadTimer").time();
+		//metricRegistry.timer("WelcomePageLoadTimer").time();
 		
 		try {
 			
@@ -103,8 +116,9 @@ public class CustomerController {
 			return "Welcome to this small REST service. It will accept a GET on /list with a request parameter lastName, and a POST to / with a JSON payload with firstName and lastName as values.";
 			
 		} finally {
-			metricRegistry.meter("WelcomePageCount").mark();
-			metricRegistry.timer("WelcomePageLoadTimer").time().stop();
+			welcomePageCount.mark();
+			//metricRegistry.meter("WelcomePageCount").mark();
+			//metricRegistry.timer("WelcomePageLoadTimer").time().stop();
 		}
 		
 		
